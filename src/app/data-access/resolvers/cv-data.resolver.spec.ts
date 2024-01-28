19456901e577e073aab7c5d25e27e3ecc9004cff
Marcin Gawski <marcin.gawski@lemisoft.pl>
2024-01-28 20:16:59 +0100

    Testy resolvera

--- src/app/data-access/resolvers/cv-data.resolver.spec.ts
+++ src/app/data-access/resolvers/cv-data.resolver.spec.ts
@@ -1,8 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-import { ResolveFn } from '@angular/router';
-
+import {
+  ActivatedRouteSnapshot,
+  ResolveFn,
+  RouterStateSnapshot,
+} from '@angular/router';
 import { cvDataResolver } from './cv-data.resolver';
-import { CvData } from '../../domain/models';
+import { CvData } from '@app/domain/models';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { first, Observable, tap } from 'rxjs';
+import { fromCvData } from '@app/data-access/state/cv-data/reducers';
+import { CvDataStateFactory } from '@app/testing/factories/state';
+import { CvDataActions } from '@app/data-access/state/cv-data/actions/cv-data.actions';
 
 describe('cvDataResolver', () => {
   const executeResolver: ResolveFn<CvData | undefined> = (
@@ -10,11 +18,48 @@ describe('cvDataResolver', () => {
   ) =>
     TestBed.runInInjectionContext(() => cvDataResolver(...resolverParameters));
 
+  let store: MockStore;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideMockStore()],
+    });
+
+    store = TestBed.inject(MockStore);
   });
 
   it('should be created', () => {
     expect(executeResolver).toBeTruthy();
+  });
+
+  it('should dispatch load action', (done) => {
+    const dispatchSpy = spyOn(store, 'dispatch');
+
+    const result = executeResolver(
+      {} as ActivatedRouteSnapshot,
+      {} as RouterStateSnapshot,
+    );
+
+    if (!(result instanceof Observable)) {
+      fail();
+
+      return;
+    }
+
+    result
+      .pipe(
+        first(),
+        tap((data: CvData | undefined) => {
+          expect(data).toBeTruthy();
+          expect(dispatchSpy).toHaveBeenCalledOnceWith(CvDataActions.load());
+
+          done();
+        }),
+      )
+      .subscribe();
+
+    store.setState({
+      [fromCvData.cvDataFeatureKey]: CvDataStateFactory.createInstance(),
+    });
   });
 });
--- src/app/data-access/resolvers/cv-data.resolver.ts
+++ src/app/data-access/resolvers/cv-data.resolver.ts
@@ -3,15 +3,18 @@ import { inject } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { CvDataActions } from '../state/cv-data/actions/cv-data.actions';
 import { cvDataFeature } from '../state/cv-data/reducers/cv-data.reducer';
-import { filter } from 'rxjs';
-import { CvData } from '../../domain/models';
+import { filter, tap } from 'rxjs';
+import { CvData } from '@app/domain/models';
 
 export const cvDataResolver: ResolveFn<CvData | undefined> = () => {
   const store: Store = inject(Store);
 
-  store.dispatch(CvDataActions.load());
-
-  return store
-    .select(cvDataFeature.selectData)
-    .pipe(filter((data: CvData | undefined) => !!data));
+  return store.select(cvDataFeature.selectData).pipe(
+    tap((data: CvData | undefined): void => {
+      if (!data) {
+        store.dispatch(CvDataActions.load());
+      }
+    }),
+    filter((data: CvData | undefined) => !!data),
+  );
 };
