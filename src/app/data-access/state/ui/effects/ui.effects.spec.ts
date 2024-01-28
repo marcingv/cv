6d3deda1641182b28d687cd108d2a01b95cf44aa
Marcin Gawski <marcin.gawski@lemisoft.pl>
2024-01-28 20:55:02 +0100

    Testy ui effects

--- src/app/data-access/state/ui/effects/ui.effects.spec.ts
+++ src/app/data-access/state/ui/effects/ui.effects.spec.ts
@@ -1,13 +1,29 @@
 import { TestBed } from '@angular/core/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
-import { Observable } from 'rxjs';
+import { first, Subject, take, tap } from 'rxjs';
 import { UiEffects } from './ui.effects';
 import { Action } from '@ngrx/store';
 import { TranslationsTestingModule } from '@app/testing/translations';
+import {
+  routerCancelAction,
+  routerErrorAction,
+  routerNavigatedAction,
+  routerRequestAction,
+} from '@ngrx/router-store';
+import {
+  RouterCancelPayload,
+  RouterErrorPayload,
+  RouterNavigatedPayload,
+  RouterRequestPayload,
+} from '@ngrx/router-store/src/actions';
+import { UiActions } from '@app/data-access/state/ui/actions/ui.actions';
+import { TranslateService } from '@ngx-translate/core';
+import { LangCode } from '@app/data-access/state/ui/models';
 
 describe('UiEffects', () => {
-  let actions$: Observable<Action>;
+  let actions$: Subject<Action>;
   let effects: UiEffects;
+  let translateService: TranslateService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -15,10 +31,71 @@ describe('UiEffects', () => {
       providers: [UiEffects, provideMockActions(() => actions$)],
     });
 
+    actions$ = new Subject<Action>();
+    translateService = TestBed.inject(TranslateService);
     effects = TestBed.inject(UiEffects);
   });
 
   it('should be created', () => {
     expect(effects).toBeTruthy();
+  });
+
+  it('should dispatch navigation started action', (done: DoneFn) => {
+    effects.navigationStarted$
+      .pipe(
+        first(),
+        tap((resultAction) => {
+          expect(resultAction).toEqual(UiActions.navigationStarted());
+
+          done();
+        }),
+      )
+      .subscribe();
+
+    actions$.next(routerRequestAction({ payload: {} as RouterRequestPayload }));
+  });
+
+  it('should dispatch navigation finished action', (done: DoneFn) => {
+    const sourceActions: Action[] = [
+      routerCancelAction({ payload: {} as RouterCancelPayload<never> }),
+      routerErrorAction({ payload: {} as RouterErrorPayload<never> }),
+      routerNavigatedAction({ payload: {} as RouterNavigatedPayload<never> }),
+    ];
+    let numberOfReactions: number = 0;
+
+    effects.navigationFinished$
+      .pipe(
+        take(sourceActions.length),
+        tap((resultAction) => {
+          expect(resultAction).toEqual(UiActions.navigationFinished());
+          numberOfReactions++;
+
+          if (numberOfReactions === sourceActions.length) {
+            done();
+          }
+        }),
+      )
+
+      .subscribe();
+
+    sourceActions.forEach((oneAction: Action) => actions$.next(oneAction));
+  });
+
+  it('should change application language', (done: DoneFn) => {
+    const changeLangSpy = spyOn(translateService, 'use');
+    const sourceAction = UiActions.changeLang({ language: LangCode.EN });
+
+    effects.changeLang$
+      .pipe(
+        first(),
+        tap(() => {
+          expect(changeLangSpy).toHaveBeenCalledOnceWith(sourceAction.language);
+
+          done();
+        }),
+      )
+      .subscribe();
+
+    actions$.next(sourceAction);
   });
 });
