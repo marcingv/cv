b69826c3cf9edad7862aa47f2e2f691c4d48c9e9
Marcin Gawski <marcin.gawski@lemisoft.pl>
2024-01-28 18:36:04 +0100

    Testy time span component

--- src/app/testing/translations/fake-translations-loader.ts
+++ src/app/testing/translations/fake-translations-loader.ts
@@ -1,9 +1,11 @@
 import { TranslateLoader } from '@ngx-translate/core';
-import { Observable } from 'rxjs';
-import { fromPromise } from 'rxjs/internal/observable/innerFrom';
+import { Observable, of } from 'rxjs';
+import { default as langPl } from '@assets/i18n/pl.json';
+import { default as langEn } from '@assets/i18n/en.json';
+import { LangCode } from '@app/data-access/state/ui/models';
 
 export class FakeTranslationsLoader implements TranslateLoader {
   public getTranslation(lang: string): Observable<object> {
-    return fromPromise(import(`../../../assets/i18n/${lang}.json`));
+    return of(lang === LangCode.PL ? langPl : langEn);
   }
 }
--- src/app/ui/components/time-span/time-span.component.spec.ts
+++ src/app/ui/components/time-span/time-span.component.spec.ts
@@ -1,16 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TimeSpanComponent } from './time-span.component';
+import { TranslationsTestingModule } from '@app/testing/translations';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslationKey } from '@app/core/translations';
 
 describe('TimeSpanComponent', () => {
   let component: TimeSpanComponent;
   let fixture: ComponentFixture<TimeSpanComponent>;
+  let translateService: TranslateService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TimeSpanComponent],
+      imports: [TimeSpanComponent, TranslationsTestingModule],
     }).compileComponents();
 
+    translateService = TestBed.inject(TranslateService);
     fixture = TestBed.createComponent(TimeSpanComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -18,5 +22,35 @@ describe('TimeSpanComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display dates range', async () => {
+    const dateFrom = new Date(2024, 1, 14);
+    const dateTo = new Date(2024, 1, 20);
+
+    fixture.componentRef.setInput('from', dateFrom);
+    fixture.componentRef.setInput('to', dateTo);
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(fixture.nativeElement.textContent).toEqual(
+      '14.02.2024 - 20.02.2024',
+    );
+  });
+
+  it('should display label for pending period', async () => {
+    const dateFrom = new Date(2024, 1, 14);
+
+    fixture.componentRef.setInput('from', dateFrom);
+    fixture.componentRef.setInput('to', undefined);
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    const tillNowLabel: TranslationKey = 'tillNow';
+    expect(fixture.nativeElement.textContent).toEqual(
+      `14.02.2024 - ${translateService.instant(tillNowLabel)}`,
+    );
   });
 });
