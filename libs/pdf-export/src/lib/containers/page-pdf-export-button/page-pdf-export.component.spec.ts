ed9a2fc128f759844ce70321bb91c3b63041540f
Marcin Gawski <marcin.gawski@lemisoft.pl>
2024-02-14 21:33:26 +0100

    Testy

--- libs/pdf-export/src/lib/containers/page-pdf-export-button/page-pdf-export.component.spec.ts
+++ libs/pdf-export/src/lib/containers/page-pdf-export-button/page-pdf-export.component.spec.ts
@@ -1,15 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PagePdfExportComponent } from './page-pdf-export.component';
+import { PdfExportService } from '../../services/pdf-export.service';
+import { of } from 'rxjs';
+
+jest.mock('../../services/pdf-export.service');
 
 describe('PagePdfExportComponent', () => {
   let component: PagePdfExportComponent;
   let fixture: ComponentFixture<PagePdfExportComponent>;
+  let exportService: PdfExportService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [PagePdfExportComponent],
+      providers: [PdfExportService],
     }).compileComponents();
 
+    exportService = TestBed.inject(PdfExportService);
     fixture = TestBed.createComponent(PagePdfExportComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +24,18 @@ describe('PagePdfExportComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should export pdf', () => {
+    component.fileName = 'my-pdf-name';
+    fixture.detectChanges();
+
+    const exportSpy = jest
+      .spyOn(exportService, 'currentPageToPdf')
+      .mockReturnValue(of());
+
+    component.export();
+
+    expect(exportSpy).toHaveBeenCalledWith(component.fileName);
   });
 });
--- libs/pdf-export/src/lib/services/pdf-export.service.spec.ts
+++ libs/pdf-export/src/lib/services/pdf-export.service.spec.ts
@@ -1,16 +1,48 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PdfExportService } from './pdf-export.service';
+import { PdfExportApiService } from '../api/pdf-export-api.service';
+import { firstValueFrom, Observable, of } from 'rxjs';
+import { saveAs } from 'file-saver';
+
+jest.mock('file-saver', () => ({
+  saveAs: jest.fn(),
+}));
 
 describe('PdfExportService', () => {
   let service: PdfExportService;
+  let api: Partial<PdfExportApiService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    api = {
+      exportPage(url: string): Observable<Blob> {
+        return of(new Blob());
+      },
+    };
+
+    TestBed.configureTestingModule({
+      providers: [{ provide: PdfExportApiService, useValue: api }],
+    });
     service = TestBed.inject(PdfExportService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call api to export current page as pdf', async () => {
+    const exportPageSpy = jest.spyOn(api, 'exportPage');
+    const currentPageUrl = 'http://localhost:4200/cv/pl';
+
+    Object.defineProperty(window, 'location', {
+      value: {
+        href: currentPageUrl,
+      },
+    });
+
+    await firstValueFrom(service.currentPageToPdf('test-file'));
+
+    expect(exportPageSpy).toHaveBeenCalledWith(currentPageUrl);
+    expect(saveAs).toHaveBeenCalledWith(new Blob(), 'test-file.pdf');
   });
 });
