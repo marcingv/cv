4aecec14fd1712321b21fefd12bf605fd604be47
Marcin Gawski <marcin.gawski@lemisoft.pl>
2024-02-15 17:32:54 +0100

    Backend: testy

--- apps/backend/src/app/cv/cv-data.service.ts
+++ apps/backend/src/app/cv/cv-data.service.ts
@@ -4,7 +4,6 @@ import { CvData, EN_CV, PL_CV } from '@gv-cv/data-models';
 @Injectable()
 export class CvDataService {
   public getData(lang: 'pl' | 'en'): Promise<CvData> {
-    console.warn('hit');
     return Promise.resolve(lang === 'en' ? EN_CV : PL_CV);
   }
 }
--- apps/backend/src/app/export/export.controller.spec.ts
+++ apps/backend/src/app/export/export.controller.spec.ts
@@ -1,9 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ExportController } from './export.controller';
 import { PdfExportService } from './pdf-export.service';
+import { Response } from 'express';
+import { BadRequestException } from '@nestjs/common';
+
+jest.mock('./pdf-export.service');
 
 describe('ExportController', () => {
   let controller: ExportController;
+  let pdfService: PdfExportService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,10 +16,47 @@ describe('ExportController', () => {
       providers: [PdfExportService],
     }).compile();
 
+    pdfService = module.get(PdfExportService);
     controller = module.get<ExportController>(ExportController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('Export PDF', () => {
+    it('should throw error for invalid request body', async () => {
+      let error: Error | undefined;
+
+      try {
+        await controller.exportPdf({ url: '' }, {} as Response);
+      } catch (err) {
+        error = err;
+      }
+
+      expect(error).toBeTruthy();
+      expect(error).toBeInstanceOf(BadRequestException);
+    });
+
+    it('should throw error when pdf export fails', async () => {
+      let error: Error | undefined;
+
+      const exportUrlSpy = jest.spyOn(pdfService, 'exportUrl');
+      exportUrlSpy.mockImplementation(() => {
+        throw new Error('Pdf Renderer Error');
+      });
+
+      try {
+        await controller.exportPdf(
+          { url: 'http://some-page.com' },
+          {} as Response,
+        );
+      } catch (err) {
+        error = err;
+      }
+
+      expect(error).toBeTruthy();
+      expect(error).toBeInstanceOf(BadRequestException);
+    });
   });
 });
