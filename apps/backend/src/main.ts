7f859b28e16b4a76b7e21a2a4d23864562a5ed95
Marcin Gawski <marcin.gawski@lemisoft.pl>
2024-02-16 15:52:57 +0100

    Konfiguracja dokumentacji swagger

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,22 +3,44 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
   });
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const swaggerEnabled = process.env.API_DOC_ENABLED === 'true';
+  if (swaggerEnabled) {
+    configureSwaggerApiDoc(app);
+  }
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
+
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
   );
+}
+
+function configureSwaggerApiDoc(app: INestApplication) {
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('CV API')
+    .setDescription('API Documentation')
+    .setVersion('1.0')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('doc', app, swaggerDocument, {
+    useGlobalPrefix: true,
+  });
 }
 
 bootstrap();
