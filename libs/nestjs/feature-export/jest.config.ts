f273f63be4e58d89e1950f43ede9ab63e8ea6910
Marcin Gawski <marcin.gawski@lemisoft.pl>
2024-02-17 15:04:28 +0100

    Biblioteka nestjs feature-export

---
+++ libs/nestjs/feature-export/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'nestjs-feature-export',
+  preset: '../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/libs/nestjs/feature-export',
+};
---
+++ libs/nestjs/feature-export/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/nestjs-feature-export.module';
