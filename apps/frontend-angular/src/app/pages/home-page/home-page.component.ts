b18d091c75557f778df6c84e36fb561834943e45
Marcin Gawski <marcin.gawski@lemisoft.pl>
2024-02-20 09:57:17 +0100

    Moved projects experience list to lib

--- apps/frontend-angular/src/app/pages/home-page/home-page.component.ts
+++ apps/frontend-angular/src/app/pages/home-page/home-page.component.ts
@@ -4,7 +4,7 @@ import { ProfileAboutComponent } from '../../features/employee/components/profil
 import { CvSectionComponent } from '@gv-cv/angular-ui-cv-section';
 import { EducationExperienceListComponent } from '@gv-cv/angular-ui-cv-education';
 import { JobExperienceListComponent } from '@gv-cv/angular-ui-cv-jobs';
-import { ProjectsExperienceListComponent } from '../../features/projects/components/projects-experience-list';
+import { ProjectsExperienceListComponent } from '@gv-cv/angular-ui-cv-projects';
 import { LanguagesExperienceListComponent } from '../../features/languages/components/languages-experience-list';
 import { HobbiesListComponent } from '@gv-cv/angular-ui-cv-hobbies';
 import { LangPickerComponent } from '@gv-cv/angular-feature-translations';
