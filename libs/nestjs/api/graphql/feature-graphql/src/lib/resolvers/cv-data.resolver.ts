8bf3da393469a26652235a8a2b3cc139074e6cef
Marcin Gawski <marcin.gawski@lemisoft.pl>
2024-02-21 16:28:14 +0100

    GraphQL queries for cv data

--- libs/nestjs/api/graphql/feature-graphql/src/lib/resolvers/cv-data.resolver.ts
+++ libs/nestjs/api/graphql/feature-graphql/src/lib/resolvers/cv-data.resolver.ts
@@ -1,13 +1,21 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { CvDataDto } from '../dtos';
 import { CvDataRepository } from '@gv-cv/nestjs-data-access-cv';
+import { LangCode } from '@gv-cv/shared-util-types';
 
 @Resolver((of: CvDataDto) => CvDataDto)
 export class CvDataResolver {
   public constructor(private readonly cvRepository: CvDataRepository) {}
 
-  @Query(() => [CvDataDto])
+  @Query(() => [CvDataDto], { name: 'getCvList' })
   async list(): Promise<CvDataDto[]> {
     return this.cvRepository.findAll();
+  }
+
+  @Query(() => CvDataDto, { nullable: true, name: 'getCvByLang' })
+  async get(
+    @Args('lang', { type: () => String }) lang: LangCode,
+  ): Promise<CvDataDto | undefined> {
+    return this.cvRepository.findByLang(lang);
   }
 }
