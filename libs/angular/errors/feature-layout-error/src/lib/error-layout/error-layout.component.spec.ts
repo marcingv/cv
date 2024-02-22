c270173e2d48eefd9694f2dffb23a6ea435200da
Marcin Gawski <marcin.gawski@lemisoft.pl>
2024-02-22 17:50:01 +0100

    Stylowanie

--- libs/angular/errors/feature-layout-error/src/lib/error-layout/error-layout.component.spec.ts
+++ libs/angular/errors/feature-layout-error/src/lib/error-layout/error-layout.component.spec.ts
@@ -1,15 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ErrorLayoutComponent } from './error-layout.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('ErrorLayoutComponent', () => {
   let component: ErrorLayoutComponent;
   let fixture: ComponentFixture<ErrorLayoutComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ErrorLayoutComponent],
-    }).compileComponents();
+    await TestBed.overrideComponent(ErrorLayoutComponent, {
+      set: {
+        imports: [],
+        schemas: [NO_ERRORS_SCHEMA],
+      },
+    })
+      .configureTestingModule({
+        imports: [ErrorLayoutComponent],
+      })
+      .compileComponents();
 
     fixture = TestBed.createComponent(ErrorLayoutComponent);
     component = fixture.componentInstance;
--- libs/angular/errors/feature-layout-error/src/lib/error-layout/error-layout.component.ts
+++ libs/angular/errors/feature-layout-error/src/lib/error-layout/error-layout.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LangPickerComponent } from '@gv-cv/angular-feature-translations';
 
 @Component({
   selector: 'gv-cv-error-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LangPickerComponent],
   templateUrl: './error-layout.component.html',
   styleUrl: './error-layout.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
