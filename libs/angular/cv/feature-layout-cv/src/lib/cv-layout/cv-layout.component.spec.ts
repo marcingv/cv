9a8493bb8b9bd5291c33b6ec2e9e152474f9159c
Marcin Gawski <marcin.gawski@lemisoft.pl>
2024-02-26 10:49:17 +0100

    Shallow testing of cv layout component

--- libs/angular/cv/feature-layout-cv/src/lib/cv-layout/cv-layout.component.spec.ts
+++ libs/angular/cv/feature-layout-cv/src/lib/cv-layout/cv-layout.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CvLayoutComponent } from './cv-layout.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('CvLayoutComponent', () => {
   let component: CvLayoutComponent;
@@ -9,7 +10,14 @@ describe('CvLayoutComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [CvLayoutComponent],
-    }).compileComponents();
+    })
+      .overrideComponent(CvLayoutComponent, {
+        set: {
+          imports: [],
+          schemas: [NO_ERRORS_SCHEMA],
+        },
+      })
+      .compileComponents();
 
     fixture = TestBed.createComponent(CvLayoutComponent);
     component = fixture.componentInstance;
