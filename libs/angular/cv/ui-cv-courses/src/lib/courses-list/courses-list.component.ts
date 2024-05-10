e36fdec22f36ce95096c5a5f5c5f2e5633e5d541
Marcin Gawski <marcin.gawski@gmail.com>
2024-05-10 08:48:05 +0200

    Generated new UI library skeleton for displaying list of courses.

---
+++ libs/angular/cv/ui-cv-courses/src/lib/courses-list/courses-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'gv-cv-courses-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './courses-list.component.html',
+  styleUrl: './courses-list.component.css',
+})
+export class CoursesListComponent {}
