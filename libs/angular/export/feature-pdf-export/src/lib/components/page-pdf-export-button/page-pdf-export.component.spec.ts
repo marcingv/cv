8a17dfe07d68ffd5ea296df9d34535e07974625d
Marcin Gawski <marcin.gawski@gmail.com>
2024-05-28 09:47:10 +0200

    Displaying toast messages in UI. Displaying toasts on PDF export success and failure events.

--- libs/angular/export/feature-pdf-export/src/lib/components/page-pdf-export-button/page-pdf-export.component.spec.ts
+++ libs/angular/export/feature-pdf-export/src/lib/components/page-pdf-export-button/page-pdf-export.component.spec.ts
@@ -1,8 +1,16 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  flush,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 import { PagePdfExportComponent } from './page-pdf-export.component';
 import { PdfExportService } from '../../services/pdf-export.service';
-import { of } from 'rxjs';
+import { delay, map, of } from 'rxjs';
 import { TranslationsTestingModule } from '@gv-cv/angular-test-translations';
+import { ToastMessage, ToastsService } from '@gv-cv/angular-ui-toasts';
+import { HttpErrorResponse } from '@angular/common/http';
 
 jest.mock('../../services/pdf-export.service');
 
@@ -10,16 +18,20 @@ describe('PagePdfExportComponent', () => {
   let component: PagePdfExportComponent;
   let fixture: ComponentFixture<PagePdfExportComponent>;
   let exportService: PdfExportService;
+  let toastsService: ToastsService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [PagePdfExportComponent, TranslationsTestingModule],
-      providers: [PdfExportService],
+      providers: [PdfExportService, ToastsService],
     }).compileComponents();
 
     exportService = TestBed.inject(PdfExportService);
+    toastsService = TestBed.inject(ToastsService);
+
     fixture = TestBed.createComponent(PagePdfExportComponent);
     component = fixture.componentInstance;
+    component.fileName = 'my-pdf-name';
     fixture.detectChanges();
   });
 
@@ -28,15 +40,104 @@ describe('PagePdfExportComponent', () => {
   });
 
   it('should export pdf', () => {
-    component.fileName = 'my-pdf-name';
-    fixture.detectChanges();
-
     const exportSpy = jest
       .spyOn(exportService, 'currentPageToPdf')
-      .mockReturnValue(of());
+      .mockReturnValue(of(new Blob([])));
 
     component.export();
 
     expect(exportSpy).toHaveBeenCalledWith(component.fileName);
   });
+
+  it('should update pending signal during successful export', fakeAsync(() => {
+    const exportSpy = jest
+      .spyOn(exportService, 'currentPageToPdf')
+      .mockReturnValue(of(new Blob([])).pipe(delay(500)));
+
+    expect(component.isPending()).toBeFalsy();
+
+    component.export();
+    expect(exportSpy).toHaveBeenCalled();
+    expect(component.isPending()).toBeTruthy();
+
+    tick(500);
+    flush();
+
+    expect(component.isPending()).toBeFalsy();
+  }));
+
+  it('should update pending signal during export failure', fakeAsync(() => {
+    const exportSpy = jest
+      .spyOn(exportService, 'currentPageToPdf')
+      .mockReturnValue(
+        of(new Blob([])).pipe(
+          delay(500),
+          map(() => {
+            throw new HttpErrorResponse({ error: 'An error', status: 500 });
+          }),
+        ),
+      );
+
+    expect(component.isPending()).toBeFalsy();
+
+    component.export();
+    expect(exportSpy).toHaveBeenCalled();
+    expect(component.isPending()).toBeTruthy();
+
+    tick(500);
+    flush();
+
+    expect(component.isPending()).toBeFalsy();
+  }));
+
+  it('should show toast message on export success', fakeAsync(() => {
+    const exportSpy = jest
+      .spyOn(exportService, 'currentPageToPdf')
+      .mockReturnValue(of(new Blob([])).pipe(delay(500)));
+
+    let toast: ToastMessage | undefined;
+    const toastShowSpy = jest
+      .spyOn(toastsService, 'show')
+      .mockImplementation((message: ToastMessage): void => {
+        toast = message;
+      });
+
+    component.export();
+    tick(500);
+    flush();
+
+    expect(exportSpy).toHaveBeenCalled();
+    expect(toastShowSpy).toHaveBeenCalled();
+    expect(toast).toBeTruthy();
+    expect(toast?.severity).toEqual('primary');
+  }));
+
+  it('should show toast message on export failure', fakeAsync(() => {
+    const exportSpy = jest
+      .spyOn(exportService, 'currentPageToPdf')
+      .mockReturnValue(
+        of(new Blob([])).pipe(
+          delay(500),
+          map(() => {
+            throw new HttpErrorResponse({ error: 'An error', status: 500 });
+          }),
+        ),
+      );
+
+    let toast: ToastMessage | undefined;
+    const toastShowSpy = jest
+      .spyOn(toastsService, 'show')
+      .mockImplementation((message: ToastMessage): void => {
+        toast = message;
+      });
+
+    component.export();
+    tick(500);
+    flush();
+
+    expect(exportSpy).toHaveBeenCalled();
+    expect(toastShowSpy).toHaveBeenCalled();
+    expect(toast).toBeTruthy();
+    expect(toast?.severity).toEqual('error');
+  }));
 });
--- libs/angular/export/feature-pdf-export/src/lib/components/page-pdf-export-button/page-pdf-export.component.ts
+++ libs/angular/export/feature-pdf-export/src/lib/components/page-pdf-export-button/page-pdf-export.component.ts
@@ -5,6 +5,7 @@ import {
   Input,
   OnDestroy,
   Output,
+  Signal,
   signal,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -15,8 +16,9 @@ import {
   IconLoadingComponent,
 } from '@gv-cv/angular-ui-icons';
 import { TranslationKey } from '@gv-cv/angular-util-translations';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { BouncingButtonComponent } from '@gv-cv/angular-ui-buttons';
+import { ToastMessage, ToastsService } from '@gv-cv/angular-ui-toasts';
 
 @Component({
   selector: 'gv-cv-page-pdf-export',
@@ -34,34 +36,73 @@ import { BouncingButtonComponent } from '@gv-cv/angular-ui-buttons';
 export class PagePdfExportComponent implements OnDestroy {
   public readonly DEFAULT_EXPORT_TIMEOUT_MS = 10 * 1000;
   public readonly DOWNLOAD_LABEL: TranslationKey = 'buttonLabels.downloadPdf';
+  public readonly SUCCESS_MESSAGE: TranslationKey = 'pdfExportToasts.success';
+  public readonly ERROR_MESSAGE: TranslationKey = 'pdfExportToasts.error';
+
+  private readonly exportService = inject(PdfExportService);
+  private readonly translateService = inject(TranslateService);
+  private readonly toastsService: ToastsService | null = inject(ToastsService, {
+    optional: true,
+  });
 
   @Input({ required: true }) public fileName!: string;
   @Input() public exportTimeoutMs: number = this.DEFAULT_EXPORT_TIMEOUT_MS;
   @Output() public exportError = new EventEmitter<{ error: Error }>();
 
-  protected pending = signal(false);
+  private pendingSignal = signal(false);
 
-  private readonly exportService = inject(PdfExportService);
   private destroyed$ = new Subject<void>();
 
+  public get isPending(): Signal<boolean> {
+    return this.pendingSignal.asReadonly();
+  }
+
   public export(): void {
-    this.pending.set(true);
+    this.pendingSignal.set(true);
     this.exportService
       .currentPageToPdf(this.fileName)
       .pipe(
         timeout({ each: this.DEFAULT_EXPORT_TIMEOUT_MS }),
         finalize(() => {
-          this.pending.set(false);
+          this.pendingSignal.set(false);
         }),
         takeUntil(this.destroyed$),
       )
       .subscribe({
-        error: (error) => this.exportError.next({ error: error }),
+        next: () => {
+          this.showSuccessToast();
+        },
+        error: (error) => {
+          this.showErrorToast();
+          this.exportError.next({ error: error });
+        },
       });
   }
 
   public ngOnDestroy(): void {
     this.destroyed$.next();
     this.destroyed$.complete();
+  }
+
+  private showSuccessToast(): void {
+    this.showToast({
+      message: this.translateService.instant(this.SUCCESS_MESSAGE),
+      severity: 'primary',
+    });
+  }
+
+  private showErrorToast(): void {
+    this.showToast({
+      message: this.translateService.instant(this.ERROR_MESSAGE),
+      severity: 'error',
+    });
+  }
+
+  private showToast(message: ToastMessage): void {
+    if (!this.toastsService) {
+      return;
+    }
+
+    this.toastsService.show(message);
   }
 }
